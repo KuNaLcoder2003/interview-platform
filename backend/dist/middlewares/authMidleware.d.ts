1e8258cf34109c5530c596ae3c776c6615041dd5
Kunal Singh <kunalindia59@gmail.com>
2025-11-10 17:02:28 +0530

    removing dist folder

--- backend/dist/middlewares/authMidleware.d.ts
+++
@@ -1,4 +0,0 @@
-import express from "express";
-declare function authMiddleware(req: any, res: express.Response, next: express.NextFunction): void;
-export default authMiddleware;
-//# sourceMappingURL=authMidleware.d.ts.map
