93eb82c2e79f14daf4d2710b3a939ed356b54dcf
Kunal Singh <kunalindia59@gmail.com>
2025-11-10 17:07:24 +0530

    Adding auth wrapper to root element

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const PUBLISHABLE_KEY = "";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </ClerkProvider>
   </React.StrictMode>
 );
